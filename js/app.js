376572632cc2e2599e17e7655eaa1a7167aea7d1
SlyLucas <lukaas_augusto@yahoo.com>
2025-03-25 11:33:53 -0300

    func return, espaço vazio

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == '') {
+        alert('Digite um nome!');
+        return;
+    }
+
     let lista = document.getElementById('lista-amigos');
     amigos.push(amigo.value);
 
